3deedcbbbdd3fa31f332f91ca161cf790f19ffac
Britton Walker <bwalker1801@gmail.com>
2022-12-23 17:33:11 -0500

    Adds TW component classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+const componentClassesPlugin = require("tailwind-component-classes");
 
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [componentClassesPlugin],
+  components: {
+    heading: {
+      h1: `text-h1 leading-[1.05] font-serif font-thin`,
+    },
+    body: {
+      1: `text-b1 font-sans font-normal`,
+    },
+    content:
+      "w-[1800px] max-w-[calc(100%-30px)] mx-auto lg:max-w-[calc(100%-60px)]",
+  },
 };
