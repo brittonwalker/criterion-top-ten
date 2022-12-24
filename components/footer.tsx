93160e14174d186493f1b80fec6b09325d40e8f4
Britton Walker <bwalker1801@gmail.com>
2022-12-24 14:14:42 -0500

    Link to project text

--- components/footer.tsx
+++ components/footer.tsx
@@ -13,6 +13,7 @@ const Footer = () => {
               target="_blank"
               rel="noreferrer nofollow"
               className="text-black"
+              aria-label="Github project link"
             >
               <GrGithub />
             </a>
