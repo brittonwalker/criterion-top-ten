a106ff816583c17b138eeb6c108a33d70a2ecf2e
Britton Walker <bwalker1801@gmail.com>
2022-12-24 12:33:55 -0500

    Use Neue Montreal font

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,16 +7,14 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        radio: ["Radio Grotesk", "sans-serif"],
+      },
+    },
   },
   plugins: [componentClassesPlugin],
   components: {
-    heading: {
-      h1: `text-h1 leading-[1.05] font-serif font-thin`,
-    },
-    body: {
-      1: `text-b1 font-sans font-normal`,
-    },
     content:
       "w-[1800px] max-w-[calc(100%-30px)] mx-auto lg:max-w-[calc(100%-60px)]",
   },
