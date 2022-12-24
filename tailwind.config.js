082747d1fef5dc61a9c5087f58ff46f018011447
Britton Walker <bwalker1801@gmail.com>
2022-12-24 12:58:18 -0500

    Adds button class

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,8 @@ module.exports = {
   },
   plugins: [componentClassesPlugin],
   components: {
+    button:
+      "border border-black rounded-full text-[1.5rem] leading-[1.875rem] hover:no-underline px-4 py-1 transform duration-300 ease-in-out",
     content:
       "w-[1800px] max-w-[calc(100%-30px)] mx-auto lg:max-w-[calc(100%-60px)]",
   },
